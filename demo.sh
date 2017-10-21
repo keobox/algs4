ebe17f772a90b9cbd431ce4b866450bbb4a0e9a0
Cesare Placanica <ceplacan@cisco.com>
2017-10-21 17:31:38 +0200

    Use Java 7 compiled and runtime.

--- demo.sh
+++ demo.sh
@@ -1,6 +1,10 @@
 #/bin/sh
 
-. java_home.env
-
-ls target || mvn clean package
-$JAVA_HOME/bin/java -cp target/algs4-1.0.0.0.jar org.keobox.algo.demo.DijkstraVsBFS algs4-data/largeEWD.txt 4
+if [ $# -eq 2 ]; then
+    JAR=algs4-1.0.0.0.jar
+    ls target/$JAR || mvn clean package
+    $JAVA_HOME/bin/java -cp target/$JAR org.keobox.algo.demo.DijkstraVsBFS algs4-data/largeEW$1.txt $2
+else
+    echo "$0 [D/G] s"
+    echo "D=dense graph, G=sparse graph, s=source vertex"
+fi
